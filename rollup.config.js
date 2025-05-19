205815b223c40dd5fbdc799ffc43f614ead0c15a
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2025-05-19 18:40:20 +0700

    Fix build config

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+const externalPeers = [
+  'react',
+  'react-native',
+  'expo-secure-store',
+];
+
+export default [
+  // 1) bundle the JS
+  {
+    input: 'src/index.ts',
+    external: externalPeers,
+    output: [
+      { file: 'dist/index.cjs', format: 'cjs', exports: 'named' },
+      { file: 'dist/index.js', format: 'esm' },
+    ],
+    plugins: [
+      resolve({ extensions: ['.js', '.ts'] }),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+
+  // 2) emit the types
+  {
+    input: 'dist/index.d.ts',
+    external: externalPeers,
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
